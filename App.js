46d48df79c71a67cb74b10bad408f90f88d13363
Deandre Baker <49622234+deandrebaker@users.noreply.github.com>
2021-05-04 23:02:20 -0400

    initial commit

--- App.js
+++ App.js
@@ -1,12 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import Axios from 'axios';
 
 export default function App() {
+  const [facts, setFacts] = useState([]);
+  const [factIndex, setFactIndex] = useState(-1);
+
+  useEffect(() => {
+    (async () => {
+      let response = await Axios.get('https://cat-fact.herokuapp.com/facts');
+      setFacts(response.data.map(fact => fact.text));
+      setFactIndex(0);
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      {
+        factIndex >= 0 ? (
+          <>
+            <View style={styles.textContainer}>
+              <Text>
+                {facts[factIndex]}
+              </Text>
+            </View>
+            <Button title="Next" style={styles.button} onPress={() => {
+              factIndex < facts.length - 1 ? setFactIndex(factIndex + 1) : setFactIndex(0);
+            }} />
+          </>
+        ) : (
+          <Text>Getting Facts...</Text>
+        )
+      }
     </View>
   );
 }
@@ -16,6 +43,13 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  textContainer: {
+    backgroundColor: '#0f0',
+    marginBottom: 30
+  },
+  button: {
+    backgroundColor: '#00f'
+  }
 });
